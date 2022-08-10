241055150b5a73e0c0f834b54f8970370b654bc4
Febryan Arota <13521120@mahasiswa.itb.ac.id>
2022-08-10 23:11:52 +0700

    Update Scholarship.js

--- src/Scholarship.js
+++ src/Scholarship.js
@@ -2,7 +2,9 @@ import React from 'react';
 import "./Scholarship.css";
 
 function Scholarship() {
-    <h1>newpage</h1>
+    return (
+        <h1>newpage</h1>
+    );
 }
 
 export default Scholarship;
